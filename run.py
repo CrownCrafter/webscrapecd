7fb6a13045ae0e6d014ad573c2bb06452010e107
CrownCrafter <ayush.mudunuru@gmail.com>
2022-11-13 10:14:41 +0100

    v1.0

---
+++ run.py
@@ -0,0 +1,5 @@
+from cdpackage import contact_finder
+query = input("Enter Query: ")
+startno = int(input("Enter start point: "))
+numqu = int(input("Enter number of results: "))
+contact_finder(query, startno, numqu)
